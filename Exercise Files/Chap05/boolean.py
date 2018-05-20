b5075d62fadc6f900fe768b78a44698c4e87c755
Yancong(Peter) Li <yancong.li@alumni.ubc.ca>
2018-05-20 00:26:16 -0700

    modified boolean.py
    modified boolean.py

--- Exercise Files/Chap05/boolean.py
+++ Exercise Files/Chap05/boolean.py
@@ -10,3 +10,33 @@ if a and b:
     print('expression is true')
 else:
     print('expression is false')
+
+print("1) -------------------------")
+    
+if a or b:
+    print('expression is true')
+else:
+    print('expression is false')
+
+print("2) -------------------------")
+
+if a is b:
+    print('expression is true')
+else:
+    print('expression is false')
+
+print("3) -------------------------")
+
+if y in x:
+    print('expression is true')
+else:
+    print('expression is false')
+
+print("4) -------------------------")
+
+if y is x[0]:
+    print(f"y is the same object as x[0]")
+print(id(y))
+print(id(x[0]))
+
+print("5) -------------------------")
