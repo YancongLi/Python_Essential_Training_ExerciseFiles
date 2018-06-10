e306700dad61de01787cd2900d17b3e6dd138764
Yancong(Peter) Li <yancong.li@alumni.ubc.ca>
2018-06-10 16:18:50 -0700

    list, tuple and dictionary
    python lists, tuplee and dictionaries

--- Exercise Files/Chap08/dict.py
+++ Exercise Files/Chap08/dict.py
@@ -4,7 +4,22 @@
 def main():
     animals = { 'kitten': 'meow', 'puppy': 'ruff!', 'lion': 'grrr',
         'giraffe': 'I am a giraffe!', 'dragon': 'rawr' }
+    animals['monkey'] = 'hahaha'
+    print('Found!' if 'lion' in animals else 'nope!')
+    print(666)
     print_dict(animals)
+    
+    print('---------------')
+    
+    dictionaryConstruct = dict(key1 = 'value1', key2 = 'value2', key3 = 'value3')
+    print_dict(dictionaryConstruct)
+    
+    print('---------------')
+    for k, v in animals.items():
+        print(f'{k} => {v}')
+
+    print('---------------')
+    for k in animals.keys(): print(k)
 
 def print_dict(o):
     for x in o: print(f'{x}: {o[x]}')
--- Exercise Files/Chap08/lists.py
+++ Exercise Files/Chap08/lists.py
@@ -2,8 +2,18 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 def main():
-    game = [ 'Rock', 'Paper', 'Scissors', 'Lizard', 'Spock' ]
+    game = [ 'Rock', 'Paper', 'Scissors', 'Lizard', 'Spock' ] #This is a list
+    print(game[0:3])
+    i = game.index('Paper')
+    game.insert(0, 'Computer')
+    game.remove('Rock')
     print_list(game)
+    print(', '.join(game))
+    print(len(game))
+    print(game.pop())
+    print(game)
+    del game[1:3]
+    print(game)
 
 def print_list(o):
     for i in o: print(i, end=' ', flush=True)
