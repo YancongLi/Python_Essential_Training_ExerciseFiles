73cc9263c352bc6dddb0f0d667befe20b9d255b6
Yancong(Peter) Li <yancong.li@alumni.ubc.ca>
2018-06-30 18:27:53 -0700

    Chapter 10: exceptions
    Finished learning Chapter 10: exceptions

--- Exercise Files/Chap10/exceptions.py
+++ Exercise Files/Chap10/exceptions.py
@@ -24,8 +24,10 @@ def inclusive_range(*args):
         i += step
 
 def main():
-    for i in inclusive_range(25):
-        print(i, end = ' ', flush = True)
-    print()
-
+    try:
+        for i in inclusive_range(25, 1, 2, 3):
+            print(i, end = ' ', flush = True)
+        print()
+    except TypeError as e:
+        print(f'range error: {e}')
 if __name__ == '__main__': main()
--- Exercise Files/Chap10/hello.py
+++ Exercise Files/Chap10/hello.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
+import sys
+
 def main():
-    print('Hello, World.')
+    try:
+        # x = int('abc')
+        y = 5/0
+    except ValueError:
+        print('I caught a ValueError')
+    # except ZeroDivisionError:
+    #     print('Don\'t divide by zero')
+    except:
+        print(f'Unknown error: {sys.exc_info()}')
+    else:
+        print(y)
+        
+        
+    print('Good job')
 
 if __name__ == '__main__': main()
