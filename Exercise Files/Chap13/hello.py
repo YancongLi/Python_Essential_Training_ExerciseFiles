dda4d3b69e339723c6df9142447ed27a8839d0a5
Yancong(Peter) Li <yancong.li@alumni.ubc.ca>
2018-08-01 22:11:18 -0700

    completed chapter 13
    completed chapter 13

--- Exercise Files/Chap13/hello.py
+++ Exercise Files/Chap13/hello.py
@@ -1,4 +1,45 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
+s = 'abcd'
+print(repr(s))# representation of the variable
+
+class bunny:
+    def __init__(self, n):
+        self._n = n
+        
+    def __repr__(self): #overriding the default function
+        return f'the number of bunnies is {self._n}'
+    
+        
+bunny = bunny(47)
+print(repr(bunny))
+print(ascii(bunny))
+
+x = (1, 2, 3, 4, 5)
+y = list(reversed(x))
+
+print(x)
+print(y)
+for i in y: print(i)
+print(sum(x, 10))
+print(max(x))
+print(min(x))
+print(any(x))#if contains any ''true elements (positive)
+print(all(x))
+for a, b in zip(x, y):
+    print(f' {a} --- {b}')
+
 print('Hello, World.')
+
+animal = ('cat', 'dog', 'rabbit')
+for i, v in enumerate(animal):
+    print(f'{i}: {v}')
+    
+    
+q = 42
+w = type(q)
+e = isinstance(q, int)
+print(q)
+print(w)
+print(e)
--- Exercise Files/Chap13/types.py
+++ Exercise Files/Chap13/types.py
@@ -8,3 +8,16 @@ print(f'x is {type(x)}')
 print(f'x is {x}')
 print(f'y is {type(y)}')
 print(f'y is {y}')
+
+z = -666
+z = abs(z)
+print(f'z is {type(z)}')
+print(f'z is {z}')
+
+h = 7
+h = divmod(h, 3)
+print(f'h is {h}')
+
+k = z + 6j
+print(f'k is {k}')
+print(f'k is {type(k)}')
